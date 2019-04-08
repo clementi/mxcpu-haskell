6ca93f66a8b5053a51017a3c9bf0a6aa6df4f086
Jeffrey Pratt <clementi@users.noreply.github.com>
2019-04-08 11:19:07 -0600

    Implement B1, B2, B3

--- app/Main.hs
+++ app/Main.hs
@@ -11,11 +11,17 @@ data CpuState = CpuState { cycles :: Int
                          , registers :: [Int]
                          } deriving (Show)
 
+toInt :: String -> Int
+toInt str = read ("0x" <> str) :: Int
+
 replaceAt :: Int -> a -> [a] -> [a]
 replaceAt _ _ [] = []
 replaceAt n value (x:xs)
   | n == 0 = value:xs
   | otherwise = x : replaceAt (n-1) value xs
+
+incCycles :: CpuState -> CpuState
+incCycles state = state = { succ (cycles state) }
 
 setCycles :: CpuState -> Int -> CpuState
 setCycles state value = state { cycles = value }
@@ -32,13 +38,21 @@ setAcc state value = state { acc = value }
 setRegister :: CpuState -> Int -> Int -> CpuState
 setRegister state index value = state { registers = replaceAt index value $ registers state }
 
+registerAt :: CpuState -> Int -> Int
+registerAt state index = index !! (registers state)
+
 interpret :: Program -> CpuState -> CpuState
 interpret [] state = state
-interpret (b:bs) state
-  | b == "B1" = CpuState { cycles = succ $ sycles state
-                         , inc =
-                         , pc = succ $ pc state
-                         , acc = }
+interpret ("B1":n:bs) state = interpret bs (setPc state n)
+interpret ("B2":idx:n:bs) state =
+  if acc state == registerAt state idx
+    then interpret bs (setPc state n)
+    else interpret bs (setPc state 3)
+interpret ("B3":value:n:bs) state =
+  if acc state == value
+    then interpret bs (setPc state n)
+    else interpret bs (setPc state 3)
+-- TODO: C0
 
 main :: IO ()
 main = do
