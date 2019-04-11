de4cf0e8ebf4f4ba8739bad303a43449e4161eba
Jeffrey Pratt <clementi@users.noreply.github.com>
2019-04-10 22:50:32 -0600

    Fiddle around with State monad

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
-import MXCPU (Program, CpuState(..), initialState, loadProgram, interpret)
+-- import MXCPU (Program, CpuState(..), initialState, loadProgram)
 
-main :: IO ()
-main = do
-  program <- loadProgram <$> getContents
-  let finalState = interpret program initialState
-  putStrLn $ show finalState
+-- main :: IO ()
+-- main = do
+--   program <- loadProgram <$> getContents
+--   let finalState = interpret program initialState
+--   putStrLn $ show finalState
+
+main = putStrLn "Hi!"
--- src/MXCPU.hs
+++ src/MXCPU.hs
@@ -1,5 +1,6 @@
-module MXCPU (Program, CpuState(..), initialState, loadProgram, interpret) where
+module MXCPU where
 
+import Control.Monad.State
 import Data.Array
 import Data.List.Split (splitOn)
 import Data.String.Utils (strip)
@@ -29,41 +30,58 @@ loadProgram s = listArray (0, length bytes - 1) bytes
   where bytes = toBytes s
         toBytes = map hex2int . splitOn " " . strip
 
-incCycles :: CpuState -> CpuState
-incCycles state = state { cycles = succ (cycles state) }
+incCycles :: State CpuState ()
+incCycles = do
+  s <- get
+  put $ s { cycles = succ $ cycles s }
 
-setCycles :: CpuState -> Int -> CpuState
-setCycles state value = state { cycles = value }
+incInc :: State CpuState ()
+incInc = do
+  s <- get
+  put $ s { inc = succ $ inc s }
 
-incInc :: CpuState -> CpuState
-incInc state = state { inc = succ (inc state) }
+decInc :: State CpuState ()
+decInc = do
+  s <- get
+  put $ s { inc = pred $ inc s }
 
-decInc :: CpuState -> CpuState
-decInc state = state { inc = pred (inc state) }
+setInc :: Int -> State CpuState ()
+setInc value = do
+  s <- get
+  put $ s { inc = value }
 
-setInc :: CpuState -> Int -> CpuState
-setInc state value = state { inc = value }
+setPc :: Int -> State CpuState ()
+setPc value = do
+  s <- get
+  put $ s { pc = value }
 
-setPc :: CpuState -> Int -> CpuState
-setPc state value = state { pc = value }
+incPc :: State CpuState ()
+incPc = do
+  s <- get
+  put $ s { pc = succ $ pc s }
 
-incPc :: CpuState -> CpuState
-incPc state = state { pc = succ (pc state) }
+incPcBy :: Int -> State CpuState ()
+incPcBy value = do
+  s <- get
+  setPc (pc s + value)
 
-incPcBy :: Int -> CpuState -> CpuState
-incPcBy n state = setPc state (n + pc state)
+setAcc :: Int -> State CpuState ()
+setAcc value = do
+  s <- get
+  put $ s { acc = value }
 
-setAcc :: CpuState -> Int -> CpuState
-setAcc state value = state { acc = value }
+addAcc :: Int -> State CpuState ()
+addAcc value = do
+  s <- get
+  put $ s { acc = value + acc s }
 
-addAcc :: CpuState -> Int -> CpuState
-addAcc state value = state { acc = value + acc state }
+setRegister :: Int -> Int -> State CpuState ()
+setRegister index value = do
+  s <- get
+  put $ s { registers = registers s // [(index, value)] }
 
-setRegister :: CpuState -> Int -> Int -> CpuState
-setRegister state index value = state { registers = registers state // [(index, value)] }
-
-registerAt :: CpuState -> Int -> Int
-registerAt state index = (registers state) ! index
+registerAt :: Int -> CpuState -> Int
+registerAt index = (! index) . registers
 
 arrayLength :: Array Int Int -> Int 
 arrayLength array = upper - lower + 1
@@ -72,64 +90,64 @@ arrayLength array = upper - lower + 1
 halt :: CpuState -> CpuState
 halt = id
 
-setPcToByte :: Int -> Program -> CpuState -> CpuState
-setPcToByte pctr program state = interpret program (incCycles (setPc state n))
-  where n = program ! (pctr + 1)
+-- setPcToByte :: Int -> Program -> State CpuState ()
+-- setPcToByte pctr program = interpret >= incCycles >>= (setPc byte)
+--   where byte = program ! (pctr + 1)
 
-jumpEqRegister :: Int -> Program -> CpuState -> CpuState
-jumpEqRegister pctr program state
-  | acc state == registerAt state index = interpret program (incCycles (setPc state n))
-  | otherwise = interpret program (incCycles (setPc state 3))
-  where index = program ! (pctr + 1)
-        n = program ! (pctr + 2)
+-- jumpEqRegister :: Int -> Program -> CpuState -> CpuState
+-- jumpEqRegister pctr program state
+--   | acc state == registerAt state index = interpret program (incCycles (setPc state n))
+--   | otherwise = interpret program (incCycles (setPc state 3))
+--   where index = program ! (pctr + 1)
+--         n = program ! (pctr + 2)
 
-jumpEqByte :: Int -> Program -> CpuState -> CpuState
-jumpEqByte pctr program state
-  | acc state == value = interpret program (incCycles (setPc state n))
-  | otherwise = interpret program (incCycles (setPc state 3))
-  where value = program ! (pctr + 1)
-        n = program ! (pctr + 2)
+-- jumpEqByte :: Int -> Program -> CpuState -> CpuState
+-- jumpEqByte pctr program state
+--   | acc state == value = interpret program (incCycles (setPc state n))
+--   | otherwise = interpret program (incCycles (setPc state 3))
+--   where value = program ! (pctr + 1)
+--         n = program ! (pctr + 2)
 
-addMemoryToAcc :: Int -> Program -> CpuState -> CpuState
-addMemoryToAcc pctr program state = interpret program (incPcBy 2 (incCycles (addAcc state registerValue)))
-  where index = program ! (pctr + 1)
-        registerValue = registerAt state index
+-- addMemoryToAcc :: Int -> Program -> CpuState -> CpuState
+-- addMemoryToAcc pctr program state = interpret program (incPcBy 2 (incCycles (addAcc state registerValue)))
+--   where index = program ! (pctr + 1)
+--         registerValue = registerAt state index
 
-addValueToAcc :: Int -> Program -> CpuState -> CpuState
-addValueToAcc pctr program state = interpret program (incPcBy 2 (incCycles (addAcc state value)))
-  where value = program ! (pctr + 1)
+-- addValueToAcc :: Int -> Program -> CpuState -> CpuState
+-- addValueToAcc pctr program state = interpret program (incPcBy 2 (incCycles (addAcc state value)))
+--   where value = program ! (pctr + 1)
 
-copyMemoryToAcc :: Int -> Program -> CpuState -> CpuState
-copyMemoryToAcc pctr program state = interpret program (incPcBy 3 (incCycles (setAcc state registerValue)))
-  where index = program ! (pctr + 1)
-        registerValue = registerAt state index
+-- copyMemoryToAcc :: Int -> Program -> CpuState -> CpuState
+-- copyMemoryToAcc pctr program state = interpret program (incPcBy 3 (incCycles (setAcc state registerValue)))
+--   where index = program ! (pctr + 1)
+--         registerValue = registerAt state index
 
-setAccToValue :: Int -> Program -> CpuState -> CpuState
-setAccToValue pctr program state = interpret program (incPcBy 2 (incCycles (setAcc state value)))
-  where value = program ! (pctr + 1)
+-- setAccToValue :: Int -> Program -> CpuState -> CpuState
+-- setAccToValue pctr program state = interpret program (incPcBy 2 (incCycles (setAcc state value)))
+--   where value = program ! (pctr + 1)
 
-copyAccToMemory :: Int -> Program -> CpuState -> CpuState
-copyAccToMemory pctr program state = interpret program (incPcBy 2 (incCycles (setRegister state index (acc state))))
-  where index = program ! (pctr + 1)
+-- copyAccToMemory :: Int -> Program -> CpuState -> CpuState
+-- copyAccToMemory pctr program state = interpret program (incPcBy 2 (incCycles (setRegister state index (acc state))))
+--   where index = program ! (pctr + 1)
 
-interpret :: Program -> CpuState -> CpuState
-interpret program state
-  | arrayLength program == 0 = state
-  | op == 0x00 = incCycles . halt $ state
-  | op == 0xB1 = setPcToByte pctr program state
-  | op == 0xB2 = jumpEqRegister pctr program state
-  | op == 0xB3 = jumpEqByte pctr program state
-  | op == 0xC0 = addMemoryToAcc pctr program state
-  | op == 0xC1 = addValueToAcc pctr program state
-  | op == 0xC2 = interpret program (incPc (incCycles (incInc state)))
-  | op == 0xC3 = interpret program (incPc (incCycles (decInc state)))
-  | op == 0xC4 = interpret program (incPc (incCycles (setInc state 0)))
-  | op == 0xC5 = interpret program (incPc (incCycles (setAcc state (inc state))))
-  | op == 0xC6 = interpret program (incPc (incCycles (setInc state (acc state))))
-  | op == 0xD0 = copyMemoryToAcc pctr program state
-  | op == 0xD1 = setAccToValue pctr program state
-  | op == 0xD2 = copyAccToMemory pctr program state
-  | otherwise = error ("unknown instruction " <> show op)
-  where op = program ! pctr
-        pctr = pc state
+-- interpret :: Program -> CpuState -> CpuState
+-- interpret program state
+--   | arrayLength program == 0 = state
+--   | op == 0x00 = incCycles . halt $ state
+--   | op == 0xB1 = setPcToByte pctr program state
+--   | op == 0xB2 = jumpEqRegister pctr program state
+--   | op == 0xB3 = jumpEqByte pctr program state
+--   | op == 0xC0 = addMemoryToAcc pctr program state
+--   | op == 0xC1 = addValueToAcc pctr program state
+--   | op == 0xC2 = interpret program (incPc (incCycles (incInc state)))
+--   | op == 0xC3 = interpret program (incPc (incCycles (decInc state)))
+--   | op == 0xC4 = interpret program (incPc (incCycles (setInc state 0)))
+--   | op == 0xC5 = interpret program (incPc (incCycles (setAcc state (inc state))))
+--   | op == 0xC6 = interpret program (incPc (incCycles (setInc state (acc state))))
+--   | op == 0xD0 = copyMemoryToAcc pctr program state
+--   | op == 0xD1 = setAccToValue pctr program state
+--   | op == 0xD2 = copyAccToMemory pctr program state
+--   | otherwise = error ("unknown instruction " <> show op)
+--   where op = program ! pctr
+--         pctr = pc state
 
