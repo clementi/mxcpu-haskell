8451b523bbcb19bed34144d8802798c0fa79e539
Jeffrey Pratt <clementi@users.noreply.github.com>
2019-04-12 11:59:31 -0600

    Pretty-print the CPU state

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,26 @@
 module Main where
 
 import Control.Monad.State (runState)
+import Data.Array (elems)
+import Data.List (intersperse)
+import Numeric (showHex)
 
 import MXCPU (Program, CpuState(..), initialState, loadProgram, interpret)
+
+toHexString :: Int -> String
+toHexString n = "0x" <> showHex n ""
+
+printState :: CpuState -> IO ()
+printState state = do
+  putStrLn ("Cycles    : " <> (show $ cycles state))
+  putStrLn ("INC       : " <> (toHexString (inc state)))
+  putStrLn ("PC        : " <> (toHexString $ pc state))
+  putStrLn ("ACC       : " <> (toHexString $ acc state))
+  putStrLn ("Registers : " <> registersString)
+  where registersString = "[" <> (concat $ intersperse "," $ map toHexString $ elems $ registers state) <> "]"
 
 main :: IO ()
 main = do
   program <- loadProgram <$> getContents
   let (_, finalState) = runState (interpret program) initialState
-  putStrLn $ show finalState
+  printState finalState
